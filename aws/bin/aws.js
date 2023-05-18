6ab43386f073e82ecfedd837abf43a793530d856
Genaro <gjmg1612@gmail.com>
2023-05-18 15:41:38 -0600

    Configuración segun error de chatGPT

--- aws/bin/aws.js
+++ aws/bin/aws.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const cdk = require('aws-cdk-lib');
-const { AwsStack } = require('../lib/aws-stack');
+const { AwsStack } = require('./lib/aws-stack');
 
 const app = new cdk.App();
 new AwsStack(app, 'AwsStack');
