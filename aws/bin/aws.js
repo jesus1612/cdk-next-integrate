70cdbefd2ca5563714fd19a7fed529f14e4179d0
Genaro <gjmg1612@gmail.com>
2023-05-18 15:50:43 -0600

    Reestructuramos el aws.js

--- aws/bin/aws.js
+++ aws/bin/aws.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
-const cdk = require('aws-cdk-lib');
-const { AwsStack } = require('../lib/aws-stack');
+const cdk, { Stack } = require('aws-cdk-lib');
+const s3 = require('aws-cdk-lib/aws-s3');
+
+class AwsStack extends Stack {
+  /**
+   * @param {cdk.App} scope
+   * @param {string} id
+   * @param {cdk.StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: false
+    });
+  }
+}
+
 
 const app = new cdk.App();
+
 new AwsStack(app, 'AwsStack');
 
 app.synth();
